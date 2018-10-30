2911ab588d8110a0446bc2fbb728ed681014ea2f
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2018-10-29 20:40:52 -0400

    tweaked api and html routes.

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,13 +1,49 @@
 var express = require("express");
 
-var router = express.Router({
-    //set api calls here
-});
+var router = express.Router();
 
-// set default "landing" page as home.html
-router.get("/", function(req, res) {
-    fs.readFile(__dirname + "/home.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    })
-});
+// Import the friends.js data
+var friends = require('../data/friends.js');
+
+
+// Export API routes
+module.exports = function(router) {
+
+	// GET route to pull in the full "friends" array of objects as JSON objects
+	router.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
+
+	// Add new user to "friends" array
+	router.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		var userInput = req.body;
+		var userResponses = userInput.scores;
+		var friendName = "";
+        var friendImage = "";
+		var totalDifference = 100; 
+        // compare user's answers to others in the "friends" array and return the closest match
+        for (var i = 0; i < friends.length; i++) {
+            // console.log('friend = ' + JSON.stringify(friends[i]));
+
+            // Compute differenes for each question
+            var diff = 0;
+            for (var j = 0; j < userResponses.length; j++) {
+                diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+            }
+
+            // If lowest difference, record the friend match
+            if (diff < totalDifference) {
+                totalDifference = diff;
+                friendName = friends[i].name;
+                friendImage = friends[i].photo;
+            }
+        }
+
+    // Add new user
+    friends.push(userInput);
+
+    // Send appropriate response
+    res.json({status: 'OK', matchName: friendName, matchImage: friendImage});
+    });
+};
--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,23 +1,15 @@
 var express = require("express");
 
-var router = express.Router({
-    //set api calls here
-});
+var router = express.Router();
 
 
 // set a get route to survey.htmlø
 router.get("/survey", function(req, res) {
-    fs.readFile(__dirname + "/survey.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    })
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
 });
 
 
 // set default catch-all "landing" page as home.html
-router.get("/", function(req, res) {
-    fs.readFile(__dirname + "/home.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    })
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
 });
